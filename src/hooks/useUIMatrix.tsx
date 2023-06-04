f144fe0d42ef60cc631ab47bba94883048474f38
Bronnick <114607031+Bronnick@users.noreply.github.com>
2023-06-04 18:07:26 +0300

    feat: added UIMatrix class

--- src/hooks/useUIMatrix.tsx
+++ src/hooks/useUIMatrix.tsx
@@ -1,17 +1,50 @@
-// class immitation
-class UIMatrix<T> {
-  private size;
-  public cells: (T | null)[][];
+import React, { MutableRefObject, ReactNode } from "react";
 
-  constructor(size: number) {
+// class immitation
+class UIMatrix {
+  private size;
+  private cells: (MutableRefObject<HTMLElement> | null)[][];
+
+  private static instance: (UIMatrix | null) = null;
+
+  private constructor(size: number) {
     this.size = size;
 
     this.cells = Array(size).fill(Array(size).fill(null));
   }
+
+  //singleton pattern method
+  public static createOnce(size: number): UIMatrix{
+    if(!this.instance){
+      //this.size = size;
+      this.instance = new UIMatrix(size);
+    }
+    return this.instance;
+  }
+
+  public getCell(i: number, j: number): (MutableRefObject<HTMLElement> | null){
+    return this.cells[i][j];
+  }
+
+  public setClassToCell(i: number, j: number, className: string): void{
+    const ref: (MutableRefObject<HTMLElement> | null) = this.getCell(i, j);
+    if(ref){
+      ref.current.className = className;
+    }
+  }
+
+  public clearClasses(): void{
+    for(let i = 0; i < this.size; i++){
+      for(let j = 0; j < this.size; j++){
+        this.setClassToCell(i, j, '');
+      }
+    }
+  }
+
 }
 
 const useUIMatrix = (size: number) => {
-  const matrix = new UIMatrix(size);
+  const matrix = UIMatrix.createOnce(20);
 
   return matrix;
 };
